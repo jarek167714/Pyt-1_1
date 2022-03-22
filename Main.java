482a4d461ce5ecf9413db6e00795a47ef2a50784
Jarek <101186879+jarek167714@users.noreply.github.com>
2022-03-22 11:29:08 +0000

    FINAL

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("H");
+    System.out.println("E");
+    System.out.println("L");
+    System.out.println("L");
+    System.out.println("L");
+    System.out.println("O");
+    System.out.println(" ");
+    System.out.println("W");
+    System.out.println("O");
+    System.out.println("R");
+    System.out.println("L");
+    System.out.println("D");
   }
 }
